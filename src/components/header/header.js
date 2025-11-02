b0e7b41c39f7e6da72b65f879bb4fae6bed763c7
mohammadtwo <bentenison61@gmail.com>
2025-11-03 02:23:43 +0330

    feat:creat showModul function

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -75,7 +75,7 @@ export function Header() {
               El({
                 element: "div",
                 className:
-                  "absolute  h-50 w-[250px] bg-[#6200ea]  grid place-items-center overflow-auto gap-2 p-4  overflow-x-scroll top-full right-0 rounded-b-xl lg:w-[500px]",
+                  "absolute  h-50 w-[250px] bg-[#6200ea] hidden  grid place-items-center overflow-auto gap-2 p-4  overflow-x-scroll top-full right-0 rounded-b-xl lg:w-[500px]",
                 children: arrFilter.map((item) => {
                   return El({
                     element: "div",
@@ -92,7 +92,112 @@ export function Header() {
             src: "../../../public/assets/plus.svg",
             alt: "plus",
           }),
-         
+           ShowMould()
+        ],
+      }),
+    ],
+  });
+}
+function ShowMould() {
+  return El({
+    element: "div",
+    className:
+      "w-full h-screen bg-[#7926eda1] absolute top-0 left-0 hidden grid place-items-center",
+    children: [
+      El({
+        element: "div",
+        className:
+          "w-[90%] bg-[#f0f0f0] border-4 gap-2 overflow-x-scroll border-[#6200ea] h-[80%] relative rounded-md flex flex-col  p-3",
+        children: [
+          El({
+            element: "div",
+            className:
+              "w-full bg-[#6200ea] absolute top-0 right-0 left-0 p-2 text-white text-center",
+            innerText: "Add new task",
+          }),
+          El({
+            element: "input",
+            type: "text",
+            className:
+              "bg-[#7926eda5] text-white rounded-md mt-8 outline-none p-3",
+            placeholder: "title",
+          }),
+          El({
+            element: "div",
+            className:
+              "flex flex-col p-3 sm:flex-row sm:justify-around bg-[#7926eda5]  rounded-md items-center justify-center",
+            children: [
+              El({
+                element: "div",
+                children: [
+                  El({
+                    element: "input",
+                    type: "date",
+                    className: "bg-[#7926eda5] text-white p-2 w-full",
+                  }),
+                ],
+              }),
+              El({
+                element: "div",
+                innerText: "priority",
+                className:
+                  "grid m-2 text-center bg-[#7926eda5] text-white rounded-md ",
+                children: [
+                  El({
+                    element: "select",
+                    className:
+                      "mt-2 bg-[#7926eda5] rounded-md p-2 outline-none",
+                    children: [
+                      El({ element: "option", innerText: "High" }),
+                      El({
+                        element: "option",
+                        innerText: "Medium",
+                      }),
+                      El({ element: "option", innerText: "Low" }),
+                    ],
+                  }),
+                ],
+              }),
+              El({
+                element: "div",
+                innerText: "status",
+                className:
+                  "grid m-2 text-center bg-[#7926eda5] text-white rounded-md",
+                children: [
+                  El({
+                    element: "select",
+                    className:
+                      "mt-2 bg-[#7926eda5] rounded-md p-2 outline-none ",
+                    children: [
+                      El({ element: "option", innerText: "ToDo" }),
+                      El({
+                        element: "option",
+                        innerText: "Doing",
+                      }),
+                      El({ element: "option", innerText: "Done" }),
+                    ],
+                  }),
+                ],
+              }),
+            ],
+          }),
+          El({
+            element: "div",
+            className: "flex-1 w-full  flex flex-col gap-2 items-center",
+            children: [
+              El({
+                element: "textarea",
+                className:
+                  " w-full p-3 bg-[#7926eda5] rounded-md h-full outline-none text-white",
+                placeholder: "description",
+              }),
+              El({
+                element: "button",
+                innerText: "submit",
+                className: "p-2 bg-[#7926ed] rounded-md text-2xl text-white cursor-pointer w-full max-w-[70%]",
+              }),
+            ],
+          }),
         ],
       }),
     ],
