6c8b4045ff545027ba52c6a02a7aa489a513f133
mohammadtwo <bentenison61@gmail.com>
2025-11-02 06:21:02 +0330

    feat: create header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,63 @@
 import { El } from "../../utils/El";
 
+export function Header() {
+  return El({
+    element: "div",
+    className: "w-full bg-[#6200ea] p-1.5 h-auto flex",
+    children: [
+      El({
+        element: "div",
+        className:"flex justify-center items-center gap-1.5 flex-1 text-white",
+        children: [
+          El({
+            element: "img",
+            src: "../../../public/assets/todo.svg",
+            alt: "logo",
+          }),
+          El({element:"h1",
+            className:"text-7xl",
+            innerText:"My To-DO Tasks",className:"flex-1"})
+        ],
+      }),
+      El({
+        element: "div",
+        className: "flex justify-end gap-2 flex-1",
+        children: [
+          El({
+            element: "img",
+            src: "../../../public/assets/search.svg",
+            alt: "search",
+            className: "sm:hidden",
+          }),
+          El({
+            element: "div",
+            className: "relative hidden sm:inline",
+            children: [
+              El({
+                element: "input",
+                placeholder: "search",
+                className: "bg-[#7926ed] pl-8 p-1 relative rounded-md",
+              }),
+              El({
+                element: "img",
+                src: "../../../public/assets/search.svg",
+                className: "absolute top-1 left-1",
+              }),
+            ],
+          }),
 
-export function Header(){
-    return El
+          El({
+            element: "img",
+            src: "../../../public/assets/filter.svg",
+            alt: "filter",
+          }),
+          El({
+            element: "img",
+            src: "../../../public/assets/plus.svg",
+            alt: "plus",
+          }),
+        ],
+      }),
+    ],
+  });
 }
--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-import './src/style.css'
+import "./style.css"
 import { Header } from './components/header/header'
 document.getElementById("app").append(Header())
